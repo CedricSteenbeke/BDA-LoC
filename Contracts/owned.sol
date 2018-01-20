23b8af2ed7eff3dcb40a0c898d0e43677435d115
Shadak <Shadak@users.noreply.github.com>
2018-01-20 12:06:39 +0100

    add owned by

---
+++ Contracts/owned.sol
@@ -0,0 +1,19 @@
+contract Owned {
+    address owner;
+
+    modifier onlyOwner() {
+        if (msg.sender == owner) _;
+    }
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+
+    function getOwner() constant returns (address){
+        return owner;
+    }
+}
